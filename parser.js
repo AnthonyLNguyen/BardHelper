b0273574ca6cd4be9748cfed329feb43b88454df
Anthony <anthonylnguyen97@gmail.com>
2017-11-22 15:28:33 -0800

    fixed bgugs

--- parser.js
+++ parser.js
@@ -1,11 +1,9 @@
 /**
  * Created by Anthony on 11/22/2017.
  */
-function myFunction() {
+function convert() {
     var notes = document.getElementById("notes").value;
     notes = replaceAll(notes,"b","♭");
-    notes = replaceAll(notes,"(+1)","+1");
-    notes = replaceAll(notes,"(-1)","-1");
     var d = ["C#","E♭","F#","G#","B♭"];
     for(var j = 0; j < d.length; j++){
         notes = replaceAll(notes,d[j]+"-1",document.getElementById(d[j]+" (-1)").value);
@@ -18,7 +16,7 @@ function myFunction() {
         notes = replaceAll(notes,a+"\\+1",document.getElementById(a+" (+1)").value);
         notes = replaceAll(notes,a,document.getElementById(a).value);
     }
-    notes = replaceAll(notes,"C"+"\\+2",document.getElementById(a+" (+2)").value);
+    notes = replaceAll(notes,"C"+"\\+2",document.getElementById("C (+2)").value);
     document.getElementById("output").value = notes;
 
 
