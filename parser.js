8fbee178c28bc14b4b75177a7a3f01b89e82e3f9
Anthony <anthonylnguyen97@gmail.com>
2017-11-22 02:15:16 -0800

    order

--- parser.js
+++ parser.js
@@ -3,6 +3,12 @@
  */
 function myFunction() {
     var notes = document.getElementById("notes").value;
+    var d = ["C#","Eb","E♭","F#","G#","Bb","B♭"];
+    for(var j = 0; j < d.length; j++){
+        notes = replaceAll(notes,d[j]+"-1",document.getElementById(d[j]+" (-1)").value);
+        notes = replaceAll(notes,d[j]+"\\+1",document.getElementById(d[j]+" (+1)").value);
+        notes = replaceAll(notes,d[j],document.getElementById(d[j]).value);
+    }
     for (var i = 65; i < 72; i++){
         var a = String.fromCharCode(i);
         notes = replaceAll(notes,a+"-1",document.getElementById(a+" (-1)").value);
@@ -10,12 +16,6 @@ function myFunction() {
         notes = replaceAll(notes,a+"-1",document.getElementById(a+" (-1)").value);
         notes = replaceAll(notes,a+"\\+1",document.getElementById(a+" (+1)").value);
         notes = replaceAll(notes,a,document.getElementById(a).value);
-    }
-    var d = ["C#","Eb","E♭","F#","G#","Bb","B♭"];
-    for(var j = 0; j < d.length; j++){
-        notes = replaceAll(notes,d[j]+"-1",document.getElementById(d[j]+" (-1)").value);
-        notes = replaceAll(notes,d[j]+"\\+1",document.getElementById(d[j]+" (+1)").value);
-        notes = replaceAll(notes,d[j],document.getElementById(d[j]).value);
     }
     document.getElementById("output").value = notes;
 
