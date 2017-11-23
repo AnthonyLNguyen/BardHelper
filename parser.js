fb84e27b572b7edb7cfccb736acbf8981aa64257
Anthony Nguyen <anthonylnguyen97@gmail.com>
2017-11-23 14:37:24 -0800

    switched ♭ and b

--- parser.js
+++ parser.js
@@ -15,13 +15,13 @@ function convert() {
         notes = replaceAll(notes,d[j]+"\\+1",document.getElementById(d[j]+" (+1)").value.toLowerCase());
         notes = replaceAll(notes,d[j],document.getElementById(d[j]).value.toLowerCase());
     }
+    notes = replaceAll(notes,"C"+"\\+2",document.getElementById("C (+2)").value.toLowerCase());
     for (var i = 65; i < 72; i++){
         var a = String.fromCharCode(i);
         notes = replaceAll(notes,a+"-1",document.getElementById(a+" (-1)").value.toLowerCase());
         notes = replaceAll(notes,a+"\\+1",document.getElementById(a+" (+1)").value.toLowerCase());
         notes = replaceAll(notes,a,document.getElementById(a).value.toLowerCase());
     }
-    notes = replaceAll(notes,"C"+"\\+2",document.getElementById("C (+2)").value.toLowerCase());
     document.getElementById("output").value = notes;
 
 
